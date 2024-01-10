c16bbdc89c141d35e90c5f9c01d0fc355558b341
morikann <mrknt0088@gmail.com>
2024-01-10 19:12:41 +0900

    feat: add navigation from home to setting

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/data/provider/api/repository_api.dart';
 import 'package:flutter_app/data/provider/shared_preferences.dart';
+import 'package:flutter_app/feature/setting/setting_page.dart';
 import 'package:flutter_app/util/logger.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -29,21 +32,51 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     final repositories = ref.watch(listPublicRepositoriesProvider);
 
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: repositories.when(
-            data: (value) => ListView.builder(
-              itemCount: value.length,
-              itemBuilder: (context, index) => ListTile(
-                title: Text(value[index].name),
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ホーム'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              unawaited(
+                Navigator.of(context).push(
+                  MaterialPageRoute<void>(
+                    builder: (context) {
+                      return const SettingPage();
+                    },
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.settings,
             ),
-            error: (error, _) => Text(error.toString()),
-            loading: CircularProgressIndicator.new,
           ),
+        ],
+      ),
+      body: Center(
+        child: repositories.when(
+          data: (value) => ListView.builder(
+            itemCount: value.length,
+            itemBuilder: (context, index) => ListTile(
+              title: Text(value[index].name),
+            ),
+          ),
+          error: (error, _) => Text(error.toString()),
+          loading: CircularProgressIndicator.new,
         ),
       ),
     );
