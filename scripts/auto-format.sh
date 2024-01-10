3808933431134ced14c750ebe16210940e07a1aa
trm11tkr <mrmrt1121@gmail.com>
2024-01-10 18:35:49 +0900

    fix: extract script to scripts/auto-format.sh

---
+++ scripts/auto-format.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+files=$(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" \))
+for file in $files; do
+# limit jobs to 5
+if [ "$(jobs -r | wc -l)" -ge 5 ]; then
+    wait "$(jobs -r -p | head -1)"
+fi
+dart fix --apply "$file" &
+done
+wait
+
+dart format "$files"
