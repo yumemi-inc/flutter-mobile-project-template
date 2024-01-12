39e56906d0bab574568746f6aaf9c13c4b2d7dd8
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2024-01-12 11:34:07 +0900

    feat: Add version check for force update

--- apps/app/lib/util/model/force_update_status.dart
+++ apps/app/lib/util/model/force_update_status.dart
@@ -6,16 +6,48 @@ part 'force_update_status.freezed.dart';
 
 @freezed
 class ForceUpdateStatus with _$ForceUpdateStatus {
-   const factory ForceUpdateStatus({
+  const factory ForceUpdateStatus({
     @Default(false) bool enabled,
   }) = _ForceUpdateStatus;
-  
+
   const ForceUpdateStatus._();
 
-  bool shouldForceUpdate({
+  static bool isForceUpdateEnabled({
+    required String currentVersion,
     required ForceUpdateTargetVersion forceUpdateTargetVersion,
   }) {
-    // TODO: バージョンの判定処理を書く
+    List<int> getVersionList(String version) {
+      final versionParts = version.split('.');
+
+      if (versionParts.length != 3) {
+        return [0, 0, 0];
+      }
+
+      try {
+        return versionParts.map(int.parse).toList();
+      } on FormatException catch (_) {
+        return [0, 0, 0];
+      }
+    }
+
+    final currentVersionList = getVersionList(currentVersion);
+    List<int> targetVersionList;
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      targetVersionList = getVersionList(forceUpdateTargetVersion.android);
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+      targetVersionList = getVersionList(forceUpdateTargetVersion.ios);
+    } else {
+      // Android / iOS 以外のプラットフォームの場合、強制バージョンアップは常に無効とする
+      return false;
+    }
+
+    for (var i = 0; i < 3; ++i) {
+      if (currentVersionList[i] < targetVersionList[i]) {
+        return true;
+      }
+    }
+
     return false;
   }
 }
