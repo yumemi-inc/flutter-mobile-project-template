c254a444e78a00f8f2fa56f590a4873d4f311e93
trm11tkr <mrmrt1121@gmail.com>
2024-01-12 18:21:23 +0900

    fix: Approach for dart format

--- scripts/auto-format.sh
+++ scripts/auto-format.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-files=$(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" \))
-for file in $files; do
+files=()
+while IFS= read -r -d $'\0' file; do
+    files+=("$file")
+done < <(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" \) -print0)
+
+for file in "${files[@]}"; do
   # limit jobs to 5
   if [ "$(jobs -r | wc -l)" -ge 5 ]; then
       wait "$(jobs -r -p | head -1)"
@@ -10,4 +14,4 @@ for file in $files; do
 done
 wait
 
-dart format "$files"
+dart format "${files[@]}"
