4973b6c3b4d98265102672f877e68411b8097eb6
Ryo Takeuchi <r_takeuchi@yumemi.co.jp>
2024-01-18 16:45:39 +0900

    docs(designsystem): dartdoc に _createAppBarTheme の使用例を記載

--- packages/cores/designsystem/lib/src/themes/app_bar_theme.dart
+++ packages/cores/designsystem/lib/src/themes/app_bar_theme.dart
@@ -7,6 +7,15 @@ AppBarTheme get lightAppBarTheme => _createAppBarTheme();
 AppBarTheme get darkAppBarTheme => _createAppBarTheme();
 
 /// Create a [AppBarTheme].
+///
+/// Different values are received as arguments for each light and dark theme,
+/// and common values are set within the function.
+///
+/// ```dart
+/// AppBarTheme get lightAppBarTheme => _createAppBarTheme(
+///       backgroundColor: lightColorScheme.primaryContainer,
+///     );
+/// ```
 AppBarTheme _createAppBarTheme({
   Color? backgroundColor,
 }) =>
