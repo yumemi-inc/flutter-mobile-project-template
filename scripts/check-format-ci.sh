869ab774a549afd158e530a1f2ee440c3ee9259d
trm11tkr <mrmrt1121@gmail.com>
2024-01-18 18:35:22 +0900

    chore: Remove unnecessary space

--- scripts/check-format-ci.sh
+++ scripts/check-format-ci.sh
@@ -5,4 +5,4 @@ while IFS= read -r -d $'\0' file; do
     files+=("$file")
 done < <(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" -o -path "*/gen/*.dart" \) -print0)
 
-dart format -o none  --set-exit-if-changed "${files[@]}"
+dart format -o none --set-exit-if-changed "${files[@]}"
