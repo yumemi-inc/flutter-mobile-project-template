c57e3c3991a74a3fe0740e11591b98c4d18cdc26
Ryo Takeuchi <r_takeuchi@yumemi.co.jp>
2024-01-18 16:42:19 +0900

    docs(designsystem): dartdoc にクラスへの参照を追加

--- packages/cores/designsystem/lib/src/themes/app_bar_theme.dart
+++ packages/cores/designsystem/lib/src/themes/app_bar_theme.dart
@@ -1,15 +1,15 @@
 import 'package:cores_designsystem/src/themes/color_schemes.g.dart';
 import 'package:flutter/material.dart';
 
-/// Create a light theme for AppBar.
+/// Create a light [AppBarTheme].
 AppBarTheme get lightAppBarTheme => _createAppBarTheme(
       backgroundColor: lightColorScheme.primaryContainer,
     );
 
-/// Create a dark theme for AppBar.
+/// Create a dark [AppBarTheme].
 AppBarTheme get darkAppBarTheme => _createAppBarTheme();
 
-/// Create a theme for AppBar.
+/// Create a [AppBarTheme].
 AppBarTheme _createAppBarTheme({
   Color? backgroundColor,
 }) =>
