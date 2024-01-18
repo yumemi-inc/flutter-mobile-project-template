6864458cf1831d9d81af34401fe95f3e5995f9b2
trm11tkr <mrmrt1121@gmail.com>
2024-01-18 14:43:48 +0900

    fix: Remove fix action from check-format

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -5,13 +5,4 @@ while IFS= read -r -d $'\0' file; do
     files+=("$file")
 done < <(find . -name "*.dart" -not \( -path "*/gen/*/*.dart" -o -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" \) -print0)
 
-for file in "${files[@]}"; do
-  # limit jobs to 5
-  if [ "$(jobs -r | wc -l)" -ge 5 ]; then
-      wait "$(jobs -r -p | head -1)"
-  fi
-  dart fix --dry-run "$file" &
-done
-wait
-
 dart format -o none  --set-exit-if-changed "${files[@]}"
