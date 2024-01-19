8e7dac925712c6c2dea733bbf392e5fbef97e6fc
trm11tkr <mrmrt1121@gmail.com>
2024-01-19 14:03:49 +0900

    feat: Add search pattern to target Dart files under dart_tool directory

--- scripts/check-format-ci.sh
+++ scripts/check-format-ci.sh
@@ -3,6 +3,6 @@
 files=()
 while IFS= read -r -d $'\0' file; do
     files+=("$file")
-done < <(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" -o -path "*/gen/*.dart" \) -print0)
+done < <(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" -o -path "*/gen/*.dart" -o -path "./.dart_tool/*" \) -print0)
 
 dart format -o none --set-exit-if-changed "${files[@]}"
