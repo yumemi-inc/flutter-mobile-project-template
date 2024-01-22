381566a8fe99c14ea87586b393313cd7a0566866
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2024-01-22 15:48:37 +0900

    feat: Add DioExtension

--- apps/app/lib/data/provider/dio.dart
+++ apps/app/lib/data/provider/dio.dart
@@ -21,13 +21,29 @@ Dio dio(DioRef ref) {
   final dio = Dio(options);
 
   dio.interceptors.add(LogInterceptor());
-  dio.interceptors.add(
-    InterceptorsWrapper(
-      onError: (e, handler) {
-        throw NetworkException.fromStatusCode(e.response?.statusCode);
-      },
-    ),
-  );
-
   return dio;
+}
+
+extension DioExtension on Dio {
+  Future<T> safeRequest<T>({
+    required Future<T> Function() request,
+  }) =>
+      _wrapDioException<T>(request);
+
+  /// Handle [DioException] and convert it to [AppException]
+  Future<T> _wrapDioException<T>(
+    Future<T> Function() request,
+  ) async {
+    try {
+      final result = await request();
+      return result;
+    } on DioException catch (e) {
+      final customException = _convertDioExceptionToNetworkException(e);
+      throw customException;
+    }
+  }
+
+  AppException _convertDioExceptionToNetworkException(DioException e) {
+    return NetworkException.fromStatusCode(e.response?.statusCode);
+  }
 }
