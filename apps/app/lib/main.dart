c917962acdf92638807ff1d8bde320900230ac28
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2024-01-22 15:51:12 +0900

    feat: Listen to AppExceptionNotifier in MainApp

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:cores_designsystem/themes.dart';
 import 'package:cores_init/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/app_exception.dart';
 import 'package:flutter_app/feature/home/ui/home_page.dart';
 import 'package:flutter_app/feature/setting/provider/theme_mode_notifier.dart';
 import 'package:flutter_app/util/logger.dart';
+import 'package:flutter_app/util/provider/app_exception_notifier_provider.dart';
+import 'package:flutter_app/util/snack_bar_manager.dart';
 import 'package:flutter_app/util/widget/custom_app_lifecyle_listerner.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -29,7 +32,20 @@ class MainApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeModeNotifierProvider);
 
+    ref.listen<AppException?>(
+      appExceptionNotifierProvider,
+      (_, appException) {
+        if (appException != null) {
+          SnackBarManager.showSnackBar(
+            'An error occurred: ${appException.message}',
+          );
+          ref.read(appExceptionNotifierProvider.notifier).consume();
+        }
+      },
+    );
+
     return MaterialApp(
+      scaffoldMessengerKey: SnackBarManager.rootScaffoldMessengerKey,
       theme: lightTheme(),
       darkTheme: darkTheme(),
       themeMode: themeMode,
