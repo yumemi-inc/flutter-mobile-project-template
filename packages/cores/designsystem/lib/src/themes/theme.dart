ce7d8077ca88ed1be745b410a97e637d288d5ee8
Ryo Takeuchi <r_takeuchi@yumemi.co.jp>
2024-01-22 11:47:15 +0900

    feat(designsystem): BottomNavigationBar のテーマを追加

--- packages/cores/designsystem/lib/src/themes/theme.dart
+++ packages/cores/designsystem/lib/src/themes/theme.dart
@@ -1,4 +1,5 @@
 import 'package:cores_designsystem/src/themes/app_bar_theme.dart';
+import 'package:cores_designsystem/src/themes/bottom_navigation_bar_theme.dart';
 import 'package:cores_designsystem/src/themes/color_schemes.g.dart';
 import 'package:flutter/material.dart';
 
@@ -6,19 +7,23 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme({ColorScheme? colorScheme}) => _createTheme(
       colorScheme: colorScheme ?? lightColorScheme,
       appBarTheme: lightAppBarTheme,
+      bottomNavigationBarTheme: lightBottomNavigationBarTheme,
     );
 
 /// Create a dark theme.
 ThemeData darkTheme({ColorScheme? colorScheme}) => _createTheme(
       colorScheme: colorScheme ?? darkColorScheme,
       appBarTheme: darkAppBarTheme,
+      bottomNavigationBarTheme: darkBottomNavigationBarTheme,
     );
 
 ThemeData _createTheme({
   required ColorScheme colorScheme,
   required AppBarTheme appBarTheme,
+  required BottomNavigationBarThemeData bottomNavigationBarTheme,
 }) =>
     ThemeData(
       colorScheme: colorScheme,
       appBarTheme: appBarTheme,
+      bottomNavigationBarTheme: bottomNavigationBarTheme,
     );
