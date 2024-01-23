7537eac00d60c4486497f63aadf413dcc55cf7be
Kotaro Kamashima <k_kamashima@yumemi.co.jp>
2024-01-23 11:00:59 +0900

    feat: Add ExceptionGeneratorApi

---
+++ packages/features/debug_mode/lib/src/data/api/provider/exception_generator_api.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'exception_generator_api.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$exceptionGeneratorApiHash() =>
+    r'581564165ab900a38af36f71223205ad48300cda';
+
+/// See also [ExceptionGeneratorApi].
+@ProviderFor(ExceptionGeneratorApi)
+final exceptionGeneratorApiProvider =
+    AutoDisposeAsyncNotifierProvider<ExceptionGeneratorApi, String>.internal(
+  ExceptionGeneratorApi.new,
+  name: r'exceptionGeneratorApiProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$exceptionGeneratorApiHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ExceptionGeneratorApi = AutoDisposeAsyncNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
