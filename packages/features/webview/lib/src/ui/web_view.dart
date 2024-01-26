b6b02417d2b77c65ca52c234c37431376dbbdd11
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-01-26 13:54:16 +0900

    fix: webviewのフォルダを移動しました

---
+++ packages/features/webview/lib/src/ui/web_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class WebView extends StatelessWidget {
+  const WebView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
