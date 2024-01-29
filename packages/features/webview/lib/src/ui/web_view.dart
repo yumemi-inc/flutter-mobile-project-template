7e62babff72aaee7442288f4c752aef4451ceefd
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-01-29 14:11:58 +0900

    add: WebViewのためのPackagesを作成しました

--- packages/features/webview/lib/src/ui/web_view.dart
+++ packages/features/webview/lib/src/ui/web_view.dart
@@ -1,11 +1,47 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebView extends StatelessWidget {
-  const WebView({super.key});
+  const WebView({
+    required this.initialUrl,
+    required this.pop,
+    super.key,
+  });
+
+  final String initialUrl;
+  final VoidCallback pop;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    InAppWebViewController? webViewController;
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (_) async {
+        if (await webViewController?.canGoBack() ?? false) {
+          await webViewController?.goBack();
+        } else {
+          pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WebView'),
+        ),
+        body: InAppWebView(
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+          initialUrlRequest: URLRequest(url: WebUri(initialUrl)),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('initialUrl', initialUrl));
+    properties.add(ObjectFlagProperty<VoidCallback>.has('pop', pop));
   }
 }
