fe1310f9783f49f89bb420f6d1709c6578a81d4b
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2024-01-30 16:19:20 +0900

    feat: Create homeNavigatorProvider

---
+++ packages/cores/navigation/lib/src/home/provider.dart
@@ -0,0 +1,7 @@
+import 'package:cores_navigation/src/home/navigator.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'provider.g.dart';
+
+@riverpod
+external HomeNavigator homeNavigator(HomeNavigatorRef ref);
