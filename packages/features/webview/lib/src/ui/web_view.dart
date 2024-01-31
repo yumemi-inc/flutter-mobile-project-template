f447a57ea9794b9407851f02fa8393b32232623e
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-02-01 08:42:14 +0900

    add: pull-to-refreshの実装をしました

--- packages/features/webview/lib/src/ui/web_view.dart
+++ packages/features/webview/lib/src/ui/web_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebView extends StatefulWidget {
+class WebView extends StatefulHookWidget {
   const WebView({
     required String initialUrl,
     required VoidCallback pop,
@@ -17,7 +19,16 @@ class WebView extends StatefulWidget {
 }
 
 class _WebViewState extends State<WebView> {
+  PullToRefreshController? _pullToRefreshController;
   InAppWebViewController? _webViewController;
+  @override
+  void initState() {
+    super.initState();
+    _pullToRefreshController = PullToRefreshController(
+      settings: PullToRefreshSettings(color: Colors.blue),
+      onRefresh: onRefresh,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +57,10 @@ class _WebViewState extends State<WebView> {
             _webViewController = controller;
           },
           initialUrlRequest: URLRequest(url: WebUri(widget._initialUrl)),
+                pullToRefreshController: _pullToRefreshController,
+                onLoadStop: (_, __) async {
+                  await _pullToRefreshController?.endRefreshing();
+                },
         ),
       ),
     );
