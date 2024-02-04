5fbe518fbdfe78bb9c95e284666eb76beedeb0c5
trm11tkr <mrmrt1121@gmail.com>
2024-02-04 13:41:30 +0900

    refactor: extract find logic to method

--- scripts/check-format-ci.sh
+++ scripts/check-format-ci.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+find_files() {
+  find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" -o -path "*/gen/*.dart" -o -path "./.dart_tool/*" \) -print0
+}
+
 files=()
 while IFS= read -r -d $'\0' file; do
     files+=("$file")
-done < <(find . -name "*.dart" -not \( -name "*.freezed.dart" -o -name "*.g.dart" -o -name "*.gen.dart" -o -path "*/gen/*.dart" -o -path "./.dart_tool/*" \) -print0)
+done < <(find_files)
 
 dart format -o none --set-exit-if-changed "${files[@]}"
