02b2be043c9bba48a9f9b75cc440f0d4e100f9da
trm11tkr <mrmrt1121@gmail.com>
2024-02-06 14:58:46 +0900

    feat: Add a documentation comment

--- packages/cores/designsystem/lib/src/theme_extensions/app_colors/app_colors.dart
+++ packages/cores/designsystem/lib/src/theme_extensions/app_colors/app_colors.dart
@@ -8,8 +8,11 @@ class AppColors extends ThemeExtension<AppColors> with _$ThemeExtensionMixin {
   const AppColors({
     required this.icon,
   });
+
+  /// Create a light [AppColors].
   factory AppColors.light() => const AppColors(icon: Color(0xFF009688));
 
+  /// Create a dark [AppColors].
   factory AppColors.dark() => const AppColors(icon: Color(0xFF80CBC4));
 
   final Color icon;
