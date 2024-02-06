86c2993a95bb31c2e283a177cfd60d78c3309932
trm11tkr <mrmrt1121@gmail.com>
2024-02-06 18:19:08 +0900

    fix: Change to forced kill option

--- scripts/check-format-ci.sh
+++ scripts/check-format-ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # for terminate any remaining background jobs when the script exits.
-trap '[ 0 -lt $(jobs | wc -l) ] && kill -HUP $(jobs -p)' EXIT
+trap '[ 0 -lt $(jobs | wc -l) ] && kill -SIGKILL $(jobs -p)' EXIT
 
 # wait for all child processes to finish and returns a non-zero status if any of them fail.
 wait_for_all(){
