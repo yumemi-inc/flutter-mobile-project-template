6f2ab19979a14aab604a7efb93bcb4465269d650
Ryo Takeuchi <r_takeuchi@yumemi.co.jp>
2024-02-29 13:24:16 +0900

    feat(core): BuildConfig を追加

---
+++ packages/cores/core/lib/src/provider/build_config_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'build_config_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$buildConfigHash() => r'9b134dae6dba8b90f2aafc427d97192a85b37e40';
+
+/// See also [buildConfig].
+@ProviderFor(buildConfig)
+final buildConfigProvider = Provider<BuildConfig>.internal(
+  buildConfig,
+  name: r'buildConfigProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$buildConfigHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef BuildConfigRef = ProviderRef<BuildConfig>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ packages/cores/core/lib/src/provider/index.dart
@@ -0,0 +1,1 @@
+export 'build_config_provider.dart';
