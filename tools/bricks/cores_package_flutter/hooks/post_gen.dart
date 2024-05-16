8f935b618a2797c1150463cc0ba84b7d6c6f8eda
morikann <mrknt0088@gmail.com>
2024-05-16 15:58:46 +0900

    feat: Update yumemi_lints version to match the project version when creating a package in mason

--- tools/bricks/cores_package_flutter/hooks/post_gen.dart
+++ tools/bricks/cores_package_flutter/hooks/post_gen.dart
@@ -3,5 +3,15 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
+  final packageName = (context.vars['package_name'] as String).snakeCase;
+  final packageDir = '${Directory.current.path}/packages/cores/$packageName';
+
   Process.runSync('melos', ['bs']);
+
+  // update yumemi_lints version
+  Process.runSync(
+    'fvm',
+    ['dart', 'run', 'yumemi_lints', 'update'],
+    workingDirectory: packageDir,
+  );
 }
