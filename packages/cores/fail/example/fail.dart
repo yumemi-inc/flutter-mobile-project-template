c54e1cf75e9c69991cce9e95ecbddf3ddad04d1d
K9i-0 <geukfyger@gmail.com>
2024-05-31 11:06:19 +0900

    fix

--- packages/cores/fail/example/fail.dart
+++ packages/cores/fail/example/fail.dart
@@ -1,6 +1,9 @@
 import 'package:cores_fail/fail.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final awesome = Awesome();
+  if (kDebugMode) {
+    print('awesome: ${awesome.isAwesome}');
+  }
 }
