809e1b6a3c8dad1a1b50e69810e6bce6655a44d4
K9i-0 <geukfyger@gmail.com>
2024-06-03 19:29:47 +0900

    test

--- packages/cores/fail/test/fail_test.dart
+++ packages/cores/fail/test/fail_test.dart
@@ -1,16 +1,7 @@
-import 'package:cores_fail/fail.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('Fail Test', () {
-      expect(awesome.isAwesome, isFalse);
-    });
+  test('fail', () {
+    expect(1, 2);
   });
 }
