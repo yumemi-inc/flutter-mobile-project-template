fcd8825a55d75472743c083a455144e8c8402e9c
Daichi Hayashi <develop@daichidaiji.com>
2024-06-10 15:14:17 +0900

    Refactor appStatus provider to use existing state instances directly

--- packages/cores/core/lib/src/app_status/provider/app_status_provider.dart
+++ packages/cores/core/lib/src/app_status/provider/app_status_provider.dart
@@ -1,6 +1,4 @@
 import 'package:cores_core/src/app_status/model/app_status.dart';
-import 'package:cores_core/src/app_status/model/force_update_status.dart';
-import 'package:cores_core/src/app_status/model/maintenance_mode_status.dart';
 import 'package:cores_core/src/app_status/provider/force_update_provider.dart';
 import 'package:cores_core/src/app_status/provider/maintenance_mode_provider.dart';
 
@@ -10,15 +8,11 @@ part 'app_status_provider.g.dart';
 
 @Riverpod(keepAlive: true)
 AppStatus appStatus(AppStatusRef ref) {
-  final maintenanceModeState = ref.watch(maintenanceModeProvider);
-  final forceUpdateState = ref.watch(forceUpdateProvider);
+  final maintenanceModeStatus = ref.watch(maintenanceModeProvider);
+  final forceUpdateStatus = ref.watch(forceUpdateProvider);
 
   return AppStatus(
-    maintenanceModeStatus: MaintenanceModeStatus(
-      enabled: maintenanceModeState.enabled,
-    ),
-    forceUpdateStatus: ForceUpdateStatus(
-      enabled: forceUpdateState.enabled,
-    ),
+    maintenanceModeStatus: maintenanceModeStatus,
+    forceUpdateStatus: forceUpdateStatus,
   );
 }
