106f1af27959278b265124b1008e327724172cef
Daichi Hayashi <d_hayashi@yumemi.co.jp>
2024-06-13 15:55:58 +0900

    rename-application-id において第1引数に Android 第2引数に iOS の ID を受け取るよう修正

--- tools/rename-application-id.sh
+++ tools/rename-application-id.sh
@@ -8,16 +8,18 @@ set -e
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 # [Step0] Check if application ID is provided
-if [ -z "$1" ]; then
-    echo "Please provide application ID to first argument."
-    echo "Usage: bash $PROJECT_ROOT/tools/rename-application-id.sh "com.example.rename""
+if [ "$#" -ne 2 ]; then
+    echo "Please provide Android application ID to first argument and iOS bundle ID to second argument."
+    echo "Usage: $PROJECT_ROOT/tools/rename-application-id.sh "com.example.android.rename" "com.example.ios.rename""
     exit 1
 fi
 
 echo "Starting application ID renaming process..."
 
-readonly NEW_APPLICATION_ID=$1
-readonly OLD_APPLICATION_ID="jp.co.yumemi.template"
+readonly NEW_ANDROID_APPLICATION_ID=$1
+readonly NEW_IOS_APPLICATION_ID=$2
+readonly OLD_ANDROID_APPLICATION_ID="jp.co.yumemi.template"
+readonly OLD_IOS_APPLICATION_ID="jp.co.yumemi.template"
 readonly OLD_NAMESPACE="jp.co.yumemi.flutter_app"
 readonly OLD_PACKAGE_NAME="jp.co.yumemi.flutter_app"
 
@@ -36,7 +38,8 @@ for file in "$FLAVOR_DIRECTORY_PATH"/*.json; do
 
     # [Options] -i extension:
     # Edit files in-place, saving backups with the specified extension. If a zero-length extension is given, no backup will be saved.
-    sed -i '' "s/\"appId\": \"$OLD_APPLICATION_ID\"/\"appId\": \"$NEW_APPLICATION_ID\"/" "$file"
+    sed -i '' "s/\"appIdAndroid\": \"$OLD_ANDROID_APPLICATION_ID\"/\"appIdAndroid\": \"$NEW_ANDROID_APPLICATION_ID\"/" "$file"
+    sed -i '' "s/\"appIdIos\": \"$OLD_IOS_APPLICATION_ID\"/\"appIdIos\": \"$NEW_IOS_APPLICATION_ID\"/" "$file"
 done
 
 # [Step2] Replace the namespace definition in build.gradle in the app module with the new application ID.
@@ -44,7 +47,7 @@ echo "[Step2] Replacing the namespace definition in build.gradle in app module w
 
 readonly APP_BUILD_GRADLE_PATH="$PROJECT_ROOT/apps/app/android/app/build.gradle"
 
-sed -i '' "s/namespace \"$OLD_NAMESPACE\"/namespace \"$NEW_APPLICATION_ID\"/" "$APP_BUILD_GRADLE_PATH"
+sed -i '' "s/namespace \"$OLD_NAMESPACE\"/namespace \"$NEW_ANDROID_APPLICATION_ID\"/" "$APP_BUILD_GRADLE_PATH"
 
 # [Step3] Create new MainActivity.kt
 readonly MAIN_ACTIVITY_START_PATH="$PROJECT_ROOT/apps/app/android/app/src/main/kotlin"
@@ -52,8 +55,8 @@ readonly MAIN_ACTIVITY_START_PATH="$PROJECT_ROOT/apps/app/android/app/src/main/k
 readonly OLD_MAIN_ACTIVITY_PATH="$MAIN_ACTIVITY_START_PATH/$(echo $OLD_PACKAGE_NAME | tr '.' '/')"
 readonly OLD_MAIN_ACTIVITY_CONTENT=$(cat "$OLD_MAIN_ACTIVITY_PATH"/MainActivity.kt)
 
-readonly NEW_MAIN_ACTIVITY_PATH="$MAIN_ACTIVITY_START_PATH/$(echo "$NEW_APPLICATION_ID" | tr '.' '/')"
-readonly NEW_MAIN_ACTIVITY_CONTENT=$(printf "%s" "$OLD_MAIN_ACTIVITY_CONTENT" | sed -e "s/package $OLD_NAMESPACE/package $NEW_APPLICATION_ID/")
+readonly NEW_MAIN_ACTIVITY_PATH="$MAIN_ACTIVITY_START_PATH/$(echo "$NEW_ANDROID_APPLICATION_ID" | tr '.' '/')"
+readonly NEW_MAIN_ACTIVITY_CONTENT=$(printf "%s" "$OLD_MAIN_ACTIVITY_CONTENT" | sed -e "s/package $OLD_NAMESPACE/package $NEW_ANDROID_APPLICATION_ID/")
 
 echo "[Step3] Creating new MainActivity.kt..."
 
