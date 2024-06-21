d65db0d4522b45e2c52fb03f661e6a7d18f770d5
K9i-0 <geukfyger@gmail.com>
2024-06-21 20:54:22 +0900

    add: analysis_options.yamlの操作を追加

--- tools/bricks/features_package/hooks/post_gen.dart
+++ tools/bricks/features_package/hooks/post_gen.dart
@@ -7,6 +7,28 @@ void run(HookContext context) {
   final useRiverpod = context.vars['use_riverpod'] as bool;
   final useFreezed = context.vars['use_freezed'] as bool;
 
+  _addDependency(
+    featureName: featureName,
+    useRiverpod: useRiverpod,
+    useFreezed: useFreezed,
+  );
+
+  _replaceAnalysisOptions(
+    featureName: featureName,
+    useRiverpod: useRiverpod,
+    useFreezed: useFreezed,
+  );
+
+  if (useRiverpod || useFreezed) {
+    Process.runSync('melos', ['bs']);
+  }
+}
+
+void _addDependency({
+  required String featureName,
+  required bool useRiverpod,
+  required bool useFreezed,
+}) {
   if (useRiverpod) {
     // 依存の追加
     Process.runSync(
@@ -42,8 +64,37 @@ void run(HookContext context) {
       workingDirectory: 'packages/features/$featureName',
     );
   }
+}
 
-  if (useRiverpod || useFreezed) {
-    Process.runSync('melos', ['bs']);
+void _replaceAnalysisOptions({
+  required String featureName,
+  required bool useRiverpod,
+  required bool useFreezed,
+}) {
+  final filePath = 'packages/features/$featureName/analysis_options.yaml';
+
+  final sb = StringBuffer();
+  sb.write('''
+include: package:yumemi_lints/flutter/3.22/recommended.yaml
+
+analyzer:
+''');
+
+  if (useRiverpod) {
+    sb.write('''
+  plugins:
+    # https://riverpod.dev/docs/introduction/getting_started#enabling-riverpod_lintcustom_lint
+    - custom_lint
+''');
   }
+  if (useFreezed) {
+    sb.write('''
+  errors:
+    # https://pub.dev/packages/freezed#install
+    invalid_annotation_target: ignore
+''');
+  }
+
+  // 変更をファイルに書き込む
+  File(filePath).writeAsStringSync(sb.toString());
 }
