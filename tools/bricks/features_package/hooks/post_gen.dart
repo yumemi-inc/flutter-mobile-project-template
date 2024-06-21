78e64faa7984cc03f4169fe6350f8c9a504363a9
K9i-0 <geukfyger@gmail.com>
2024-06-21 20:26:20 +0900

    add: post hookで依存の追加

--- tools/bricks/features_package/hooks/post_gen.dart
+++ tools/bricks/features_package/hooks/post_gen.dart
@@ -1,5 +1,49 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  // TODO: add post-generation logic.
+  final featureName = context.vars['feature_name'] as String;
+  final useRiverpod = context.vars['use_riverpod'] as bool;
+  final useFreezed = context.vars['use_freezed'] as bool;
+
+  if (useRiverpod) {
+    // 依存の追加
+    Process.runSync(
+      'dart',
+      [
+        'pub',
+        'add',
+        'flutter_hooks',
+        'hooks_riverpod',
+        'riverpod_annotation',
+        'dev:build_runner',
+        'dev:riverpod_generator',
+        'dev:riverpod_lint',
+        'dev:custom_lint'
+      ],
+      workingDirectory: 'packages/features/$featureName',
+    );
+  }
+
+  if (useFreezed) {
+    // 依存の追加
+    Process.runSync(
+      'dart',
+      [
+        'pub',
+        'add',
+        'freezed_annotation',
+        'json_annotation',
+        'dev:build_runner',
+        'dev:freezed',
+        'dev:json_serializable',
+      ],
+      workingDirectory: 'packages/features/$featureName',
+    );
+  }
+
+  if (useRiverpod || useFreezed) {
+    Process.runSync('melos', ['bs']);
+  }
 }
