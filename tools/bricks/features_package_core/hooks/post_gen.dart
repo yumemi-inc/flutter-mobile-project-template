7bc40fa3ce40b9c7224ce44720760d4283e802a2
K9i-0 <geukfyger@gmail.com>
2024-06-27 09:45:24 +0900

    add: main.dartにL10を追加

--- tools/bricks/features_package_core/hooks/post_gen.dart
+++ tools/bricks/features_package_core/hooks/post_gen.dart
@@ -5,7 +5,23 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   final featureName = context.vars['feature_name'] as String;
 
-  // appにfeatureの依存を追加
+  _addFeatureDependency(featureName: featureName);
+  _addL10n(featureName: featureName);
+
+  Process.runSync(
+    'melos',
+    ['bs'],
+  );
+  Process.runSync(
+    'melos',
+    ['run', 'regenerate_by_using_gen_l10n'],
+  );
+}
+
+/// appにfeatureの依存を追加
+void _addFeatureDependency({
+  required String featureName,
+}) {
   Process.runSync(
     'dart',
     [
@@ -20,13 +36,49 @@ void run(HookContext context) {
     ],
     workingDirectory: 'apps/app',
   );
+}
 
-  Process.runSync(
-    'melos',
-    ['bs'],
+/// appのmain.dartにl10nのimportなどを追加
+void _addL10n({
+  required String featureName,
+}) {
+  // main.dartファイルのパス
+  String filePath = 'apps/app/lib/main.dart';
+
+  // main.dartファイルを読み込み
+  String fileContent = File(filePath).readAsStringSync();
+
+  // 最初の行に追加するimport文
+  String importStatement =
+      "import 'package:features_$featureName/l10n.dart';\n";
+
+  // 追加するコード
+  String newLocalizationsDelegates = '''
+        ...${featureName.pascalCase}L10n.localizationsDelegates,
+  ''';
+  String newSupportedLocales = '''
+        ...${featureName.pascalCase}L10n.supportedLocales,
+  ''';
+
+  // import文を最初に追加
+  fileContent = importStatement + fileContent;
+
+  // localizationsDelegatesに追加
+  fileContent = fileContent.replaceFirst(
+    'localizationsDelegates: const [',
+    'localizationsDelegates: const [\n$newLocalizationsDelegates',
   );
-  Process.runSync(
-    'melos',
-    ['run', 'regenerate_by_using_gen_l10n'],
+
+  // supportedLocalesに追加
+  fileContent = fileContent.replaceFirst(
+    'supportedLocales: const [',
+    'supportedLocales: const [\n$newSupportedLocales',
   );
+
+  // main.dartファイルを再度書き込み
+  File(filePath).writeAsStringSync(fileContent);
+
+  // フォーマット
+  // importのソート未対応
+  Process.runSync('dart', ['format', filePath]);
 }
