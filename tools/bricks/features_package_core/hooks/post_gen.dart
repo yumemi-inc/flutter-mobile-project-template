6f7b799fd5cf774fbfe0fb8a18d49ea167c7f715
K9i-0 <geukfyger@gmail.com>
2024-06-27 09:21:43 +0900

    add: 新しい機能 をappの依存に追加

--- tools/bricks/features_package_core/hooks/post_gen.dart
+++ tools/bricks/features_package_core/hooks/post_gen.dart
@@ -3,6 +3,24 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
+  final featureName = context.vars['feature_name'] as String;
+
+  // appにfeatureの依存を追加
+  Process.runSync(
+    'dart',
+    [
+      'pub',
+      'add',
+      // Dartバージョンが上がったらこっちの書き方に変更する
+      // https://dart.dev/tools/pub/cmd/pub-add#path-1
+      // '\'features_$featureName:{"path":"../../packages/features/$featureName"}\'',
+      'features_$featureName',
+      '--path',
+      '../../packages/features/$featureName',
+    ],
+    workingDirectory: 'apps/app',
+  );
+
   Process.runSync(
     'melos',
     ['bs'],
