092c82e811afd662b5691b5854ec4c91de675c22
Ryo Takeuchi <r_takeuchi@yumemi.co.jp>
2024-08-05 17:13:16 +0900

    feat: 強制アップデート機能のパッケージを追加

---
+++ packages/features/force_update/lib/src/app_status/provider/force_update_version_provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore
+
+part of 'force_update_version_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$forceUpdateVersionHash() =>
+    r'2341114642ae345a8a1bd0d7b43176d27b88769e';
+
+/// See also [ForceUpdateVersion].
+@ProviderFor(ForceUpdateVersion)
+final forceUpdateVersionProvider =
+    NotifierProvider<ForceUpdateVersion, ForceUpdateTargetVersion>.internal(
+  ForceUpdateVersion.new,
+  name: r'forceUpdateVersionProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$forceUpdateVersionHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ForceUpdateVersion = Notifier<ForceUpdateTargetVersion>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
