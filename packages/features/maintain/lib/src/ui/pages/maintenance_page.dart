7c4d8e933112bf81a65eef707294b747b0627504
Kojiro Yokota <k_yokota@yumemi.co.jp>
2024-08-09 17:19:21 +0900

    feat: メンテナンス関連の実装をfeatures_maintainパッケージに移植した

--- packages/features/maintain/lib/src/ui/pages/maintenance_page.dart
+++ packages/features/maintain/lib/src/ui/pages/maintenance_page.dart
@@ -1,6 +1,6 @@
 import 'package:cores_core/app_status.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MaintenancePage extends ConsumerWidget {
   const MaintenancePage({super.key});
--- packages/features/maintain/lib/ui.dart
+++ packages/features/maintain/lib/ui.dart
@@ -1,3 +1,1 @@
-export 'src/ui/components/maintain_text.dart';
-export 'src/ui/pages/detail/maintain_detail_page.dart';
-export 'src/ui/pages/list/maintain_list_page.dart';
+export 'src/ui/pages/maintenance_page.dart';
