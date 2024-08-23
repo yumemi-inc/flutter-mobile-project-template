4173cb4176f7051e437056e6e32df10a916646a8
Ryo Takeuchi <r_takeuchi@yumemi.co.jp>
2024-08-23 20:14:03 +0900

    refactor: cores_core から AppStatus 関連の記述を削除

--- packages/cores/core/lib/app_status.dart
+++
@@ -1,4 +0,0 @@
-export 'src/app_status/model/app_status.dart';
-export 'src/app_status/model/maintenance_mode_status.dart';
-export 'src/app_status/provider/app_status_provider.dart';
-export 'src/app_status/provider/maintenance_mode_provider.dart';
