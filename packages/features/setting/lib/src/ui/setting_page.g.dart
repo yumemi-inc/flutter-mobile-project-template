60f236cf9746c44fbf58d63f9d6d58d901a90163
morikann <mrknt0088@gmail.com>
2025-01-09 14:14:42 +0900

    refactor(features_setting): settingPageNavigator の定義を setting_page.dart に移行する

--- packages/features/setting/lib/src/ui/setting_page.g.dart
+++ packages/features/setting/lib/src/ui/setting_page.g.dart
@@ -2,7 +2,7 @@
 
 // ignore_for_file: type=lint, duplicate_ignore, deprecated_member_use
 
-part of 'navigator_provider.dart';
+part of 'setting_page.dart';
 
 // **************************************************************************
 // RiverpodGenerator
