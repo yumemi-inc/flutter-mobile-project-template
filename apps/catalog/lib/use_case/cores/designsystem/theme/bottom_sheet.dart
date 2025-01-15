5748b9c26e7031cac2e46a82e409e9872c69d3c8
karas1910 <onenight1910@gmail.com>
2025-01-16 05:59:33 +0900

    feat: add BottomSheet

---
+++ apps/catalog/lib/use_case/cores/designsystem/theme/bottom_sheet.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+@UseCase(
+  name: 'BottomSheet',
+  type: BottomSheet,
+  path: '[Theme Preview]',
+)
+Widget bottomSheet(BuildContext context) => Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            unawaited(
+              showModalBottomSheet<void>(
+                context: context,
+                builder: (context) {
+                  return SizedBox(
+                    height: 200,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Text('Modal BottomSheet'),
+                          ElevatedButton(
+                            child: const Text('Close BottomSheet'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+          child: const Text('Show BottomSheet'),
+        ),
+      ),
+    );
