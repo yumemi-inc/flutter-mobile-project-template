1e00baca0e417dc708dc3edd0de39e1641f915b8
r0227n <developryo@gmail.com>
2025-04-24 15:42:02 +0900

    add: yamlとlockを比較し、違いを出力可能にした

--- tools/diff_yaml.dart
+++ tools/diff_yaml.dart
@@ -1,51 +1,124 @@
 import 'dart:io';
+import 'dart:convert';
 import 'package:path/path.dart';
 import 'package:yaml/yaml.dart';
 import 'utils/path.dart';
 
-/// Output the changes in pubspeck.lock in a table format.
+extension YamlMapToJson on YamlMap {
+  Map<String, dynamic> toJson() {
+    final jsonString = jsonEncode(this);
+    return jsonDecode(jsonString);
+  }
+}
+
 Future<void> main(List<String> args) async {
   final yamlPath = args[0];
+  final lockPath = args[1];
 
   final rootDir = fetchGitRootDirPath();
-  final lockFile = File(join(rootDir, yamlPath));
-  final currentYaml = loadYaml(lockFile.readAsStringSync());
 
+  final yamlFile = File(join(rootDir, yamlPath));
+  final currentYaml = loadYaml(yamlFile.readAsStringSync()) as YamlMap;
+  final lockFile = File(join(rootDir, lockPath));
+  final currentLock = loadYaml(lockFile.readAsStringSync()) as YamlMap;
+
+  final compareValue = await diffLockContent(lockPath, currentLock);
+
+  final rows = [];
+
+  if (compareValue.isNotEmpty) {
+    rows.add('''
+## pubspec.lock has been changed
+
+| Change | Diff |
+| :---   | :--- |
+    ''');
+
+    for (final entry in compareValue.entries) {
+      final row = '|' + entry.key + '|' + entry.value.toString() + '|' + '\n';
+      rows.add(row);
+    }
+
+    rows.add('''
+    ---
+    ''');
+  }
+
+  final diffVersions = diffPkgVersion(currentYaml, currentLock);
+
+  if (diffVersions.isNotEmpty) {
+    rows.add('''
+## Compare pubspec.yaml and pubspec.lock. Packages with different version notations
+      
+| package | pubspec.yaml | pubspec.lock | 
+| :---    | :---         |  :---        |
+''');
+
+    for (final diff in diffVersions) {
+      final row = '|' +
+          diff['package'] +
+          '|' +
+          diff['pubspec.yaml'] +
+          '|' +
+          diff['pubspec.lock'] +
+          '|' '\n';
+      rows.add(row);
+    }
+  }
+
+  stdout.writeln(rows.join('').trim());
+}
+
+/// 実行ブランチとorigin/mainのpubspec.lockの内容を比較し、表記が異なるものを返す
+Future<Map<String, dynamic>> diffLockContent(String path, YamlMap lock) async {
   final exitCode = await Process.run('git', ['fetch', 'origin', 'main'])
       .then((e) => e.exitCode);
   if (exitCode != 0) {
     throw Exception('Failed to execute git fetch origin main');
   }
 
-  final (mainLock, error) =
-      await Process.run('git', ['show', 'origin/main:$yamlPath'])
+  final (success, error) =
+      await Process.run('git', ['show', 'origin/main:$path'])
           .then((value) => (value.stdout, value.stderr));
 
   if (error.isNotEmpty) {
-    throw Exception('Failed to execute git show main:$yamlPath / $stderr');
+    throw Exception('Failed to execute git show main:$path / $stderr');
   }
-  final mainYaml = loadYaml(mainLock);
+  final mainLock = loadYaml(success);
 
-  final compareValue = compareMap(mainYaml, currentYaml);
+  return compareMap(mainLock, lock);
+}
 
-  final rows = [
-    '''
-  ## pubspec.lock has been changed
+/// pubspec.yamlとpubspec.lockの内容を参照し、versionの表記が異なるものを返す
+Iterable<Map<String, dynamic>> diffPkgVersion(
+  YamlMap yaml,
+  YamlMap lock,
+) {
+  final yamlJson = yaml.toJson();
 
-  | Change | Diff |
-  | :---   | :--- |
-  '''
-  ];
-  for (final entry in compareValue.entries) {
-    final row = '|' + entry.key + '|' + entry.value.toString() + '|' + '\n';
-    rows.add(row);
-  }
+  final pkgYaml = <String, dynamic>{}
+    ..addAll(yamlJson['dev_dependencies'])
+    ..addAll(yamlJson['dependency_overrides'])
+    ..addAll(yamlJson['melos']['command']['bootstrap']['dependencies'])
+    ..addAll(yamlJson['melos']['command']['bootstrap']['dev_dependencies']);
+  final lockPackages = Map<String, dynamic>.from(lock['packages']);
 
-  if (rows.length == 1) {
-    return;
-  }
+  return lockPackages.entries.where((lockPkg) {
+    final name = lockPkg.key;
 
-  stdout.writeln(rows.join('').trim());
+    if (pkgYaml.containsKey(name)) {
+      final yamlVersion = pkgYaml[name].replaceFirst('^', '');
+      final lockVersion = lockPkg.value['version'].toString();
+
+      return yamlVersion != lockVersion;
+    }
+
+    return false;
+  }).map((e) => {
+        "package": e.key,
+        "pubspec.yaml": pkgYaml[e.key],
+        "pubspec.lock": e.value['version']
+      });
 }
 
 /// Compares [Maps] and returns a [Map<String, dynami>] of the elements that differ between [previous] and [current].
