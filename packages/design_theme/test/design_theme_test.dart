51453c7875df3c22b50b828168ec776505d675d4
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-05-20 15:25:37 +0900

    feat: design_theme パッケージを作成
    dart create -t package packages/design_theme --force

---
+++ packages/design_theme/test/design_theme_test.dart
@@ -0,0 +1,16 @@
+import 'package:design_theme/design_theme.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
