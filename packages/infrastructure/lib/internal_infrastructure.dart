e57b93a10c34228b4dadc65a936cf9eff7e1a80b
karas1910 <onenight1910@gmail.com>
2025-05-27 11:57:27 +0900

    feat: テーマモード管理のためのリポジトリを追加
    - pubspec.yamlにflutter依存関係を追加
    - internal_domain_logic.dartにテーマモードリポジトリをエクスポート
    - 新規ファイルとしてThemeModeRepositoryとその実装を追加
    - ThemeModeRepositoryImplでSharedPreferenceを利用したテーマモードの取得と保存機能を実装

--- packages/infrastructure/lib/internal_infrastructure.dart
+++ packages/infrastructure/lib/internal_infrastructure.dart
@@ -10,5 +10,6 @@ library;
 export 'src/internal_infrastructure_base.dart';
 export 'src/shared_preferences/shared_preference_data_source.dart';
 export 'src/shared_preferences/shared_preferences_keys.dart';
+export 'src/theme_mode/theme_mode_repository_impl.dart';
 
 // TODO: Export any libraries intended for clients of this package.
