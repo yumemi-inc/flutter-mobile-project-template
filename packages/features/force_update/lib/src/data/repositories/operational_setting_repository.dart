b9bb072699dafe369054345d8709808f00a98fe5
r0227n <developryo@gmail.com>
2025-06-13 15:48:42 +0900

    chore: testデータを返すようにした

--- packages/features/force_update/lib/src/data/repositories/operational_setting_repository.dart
+++ packages/features/force_update/lib/src/data/repositories/operational_setting_repository.dart
@@ -1,4 +1,5 @@
 import 'package:features_force_update/src/domain/models/operational_settings.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 class OperationalSettingRepository {
   /// [OperationalSettings]を取得する
@@ -14,13 +15,20 @@ class OperationalSettingRepository {
     );
   }
 
-  // TODO: MaintenancePolicyを取得する
-  Future<MaintenancePolicy> getMaintenancePolicy() async {
-    return const MaintenancePolicy.disabled();
+  // TODO: テストデータのため、案件に合わせて修正する
+  Future<MaintenancePolicy> getMaintenancePolicy() {
+    return Future.value(const MaintenancePolicy.enabled(message: 'Test'));
   }
 
-  // TODO: ForceUpdatePolicyを取得する
-  Future<ForceUpdatePolicy> getForceUpdatePolicy() async {
-    return const ForceUpdatePolicy.disabled();
+  // TODO: テストデータのため、案件に合わせて修正する
+  Future<ForceUpdatePolicy> getForceUpdatePolicy() {
+    return Future.value(
+      ForceUpdatePolicy.enabled(
+        minimumVersions: RequiredVersions(
+          ios: Version.none,
+          android: Version.none,
+        ),
+      ),
+    );
   }
 }
