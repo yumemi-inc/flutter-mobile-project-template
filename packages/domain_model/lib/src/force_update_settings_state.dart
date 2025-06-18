9ff151cfd7f939925b052153e56c5938a413a6ac
r0227n <developryo@gmail.com>
2025-06-18 09:20:00 +0900

    chore: ForceUpdateSettingsState をsealed から abstractに変更

--- packages/domain_model/lib/src/force_update_settings_state.dart
+++ packages/domain_model/lib/src/force_update_settings_state.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'force_update_settings_state.freezed.dart';
 
 @freezed
-sealed class ForceUpdateSettingsState with _$ForceUpdateSettingsState {
+abstract class ForceUpdateSettingsState with _$ForceUpdateSettingsState {
   const factory ForceUpdateSettingsState({
     @Default(false) bool enabled,
   }) = _ForceUpdateSettingsState;
