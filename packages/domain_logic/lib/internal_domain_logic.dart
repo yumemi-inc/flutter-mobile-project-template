1126e8a4852d46703e8f14f5887fe51d13281e4c
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2025-08-20 00:42:41 +0900

    feat: domain_logicパッケージにoperational settingsリポジトリインターフェースを追加
    - OperationalSettingsRepositoryをabstract interface classとして作成
    - 運用設定集約ルートのリポジトリ抽象化

--- packages/domain_logic/lib/internal_domain_logic.dart
+++ packages/domain_logic/lib/internal_domain_logic.dart
@@ -4,6 +4,7 @@
 library;
 
 export 'src/internal_domain_logic_base.dart';
+export 'src/operational_settings/operational_settings_repository.dart';
 export 'src/theme_mode/theme_mode_repository.dart';
 
 // TODO: Export any libraries intended for clients of this package.
