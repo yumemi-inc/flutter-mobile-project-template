250dab274d1bf198a3c4b626ceccc869cfb9e9cd
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-20 23:50:14 +0900

    refactor: cores/designsystemパッケージを削除し、依存関係を整理
    - packages/cores/designsystemパッケージを完全削除
    - 関連するインポートパスを更新
    - 各アプリケーションのpubspec.yamlから不要な依存関係を削除
    - GitHubラベル設定を更新

--- packages/samples/github_repository/lib/src/ui/github_repository_list_page.dart
+++ packages/samples/github_repository/lib/src/ui/github_repository_list_page.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:cores_designsystem/components.dart';
+import 'package:internal_design_ui/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
