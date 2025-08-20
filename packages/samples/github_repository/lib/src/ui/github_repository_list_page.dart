160dba2258ee76771ca5595c72eede6fe9561b45
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-21 07:31:53 +0900

    fix: 警告を修正

--- packages/samples/github_repository/lib/src/ui/github_repository_list_page.dart
+++ packages/samples/github_repository/lib/src/ui/github_repository_list_page.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 
-import 'package:internal_design_ui/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:internal_design_ui/components.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:samples_github_repository/src/data/provider/scroll_notifier.dart';
 import 'package:samples_github_repository/src/domain/model/github_repository.dart';
