20f36655eeb15f37a7274160e0c946900ef4d428
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-21 09:23:11 +0900

    feat: 新アーキテクチャ用のプロバイダーを追加
    - SharedPreferencesとSharedPreferenceDataSourceのプロバイダーを追加
    - ThemeModeRepositoryのプロバイダーを追加
    - ThemeModeNotifierのプロバイダーを追加

---
+++ apps/app/lib/composition_root/data_sources/shared_preference_data_source.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, duplicate_ignore, deprecated_member_use
+
+part of 'shared_preference_data_source.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sharedPreferencesHash() => r'91d3d8d16af3d747cec711b8a095a63e20df9b7c';
+
+/// See also [sharedPreferences].
+@ProviderFor(sharedPreferences)
+final sharedPreferencesProvider =
+    AutoDisposeProvider<SharedPreferences>.internal(
+      sharedPreferences,
+      name: r'sharedPreferencesProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$sharedPreferencesHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SharedPreferencesRef = AutoDisposeProviderRef<SharedPreferences>;
+String _$sharedPreferenceDataSourceHash() =>
+    r'3de5942f9ab4b5bb10a2b14640f80de60ca672f6';
+
+/// See also [sharedPreferenceDataSource].
+@ProviderFor(sharedPreferenceDataSource)
+final sharedPreferenceDataSourceProvider =
+    AutoDisposeProvider<SharedPreferenceDataSource>.internal(
+      sharedPreferenceDataSource,
+      name: r'sharedPreferenceDataSourceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$sharedPreferenceDataSourceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SharedPreferenceDataSourceRef =
+    AutoDisposeProviderRef<SharedPreferenceDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
