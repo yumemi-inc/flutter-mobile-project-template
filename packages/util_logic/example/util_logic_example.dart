be60e4fdf52e075871ebbb2d87f18489e61b1869
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-27 15:54:18 +0900

    feat: util_lgic, util_ui パッケージを仮作成

---
+++ packages/util_logic/example/util_logic_example.dart
@@ -0,0 +1,6 @@
+import 'package:util_logic/util_logic.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/util_ui/example/util_ui_example.dart
@@ -0,0 +1,6 @@
+import 'package:util_ui/util_ui.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
