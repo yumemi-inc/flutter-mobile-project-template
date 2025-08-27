9000a74001676c486fe4faf7f5c57bdff35491eb
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-27 14:23:51 +0900

    refactor: 使っていない FakeBuildConfig を削除

--- apps/app/lib/configuration/build_config.dart
+++ apps/app/lib/configuration/build_config.dart
@@ -9,30 +9,3 @@ abstract interface class BuildConfig {
   abstract final Flavor flavor;
   abstract final String? installerStore;
 }
-
-final class FakeBuildConfig implements BuildConfig {
-  FakeBuildConfig({
-    this.appName = 'Fake App',
-    this.packageName = 'com.example.fake_app',
-    this.version = '1.0.0',
-    this.buildNumber = '1',
-    this.buildSignature = '1',
-    this.flavor = Flavor.dev,
-    this.installerStore = 'fake_store',
-  });
-
-  @override
-  final String appName;
-  @override
-  final String packageName;
-  @override
-  final String version;
-  @override
-  final String buildNumber;
-  @override
-  final String buildSignature;
-  @override
-  final Flavor flavor;
-  @override
-  final String? installerStore;
-}
