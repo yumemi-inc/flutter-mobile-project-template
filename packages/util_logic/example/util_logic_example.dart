764c6a1cb0f69cba235c59f6d5a58a8337d4c5bc
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-27 15:56:24 +0900

    fix: 不要なファイルを削除

--- packages/util_logic/example/util_logic_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:util_logic/util_logic.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- packages/util_ui/example/util_ui_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:util_ui/util_ui.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
