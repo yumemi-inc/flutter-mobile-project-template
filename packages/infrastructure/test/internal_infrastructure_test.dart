d6877e7016de580b9d3de3eed28621510b864a5b
Tatsuya Okayama (blendthink) <t_okayama@yumemi.co.jp>
2025-08-28 14:26:18 +0900

    fix: テストを問題なく実行できるように flutter_test に差し替え

--- packages/infrastructure/test/internal_infrastructure_test.dart
+++ packages/infrastructure/test/internal_infrastructure_test.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:internal_infrastructure/internal_infrastructure.dart';
-import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
