c01fdb76af73b7e1a6c94da60245bc503932852f
Kojiro Yokota <k_yokota@yumemi.co.jp>
2025-12-24 13:07:42 +0900

    fix: build.gradleは存在しないので、パスをbuild.gradle.ktsに修正した

--- tools/rename-application-id.sh
+++ tools/rename-application-id.sh
@@ -45,7 +45,7 @@ done
 # [Step2] Replace the namespace definition in build.gradle in the app module with the new application ID.
 echo "[Step2] Replacing the namespace definition in build.gradle in app module with new application ID..."
 
-readonly APP_BUILD_GRADLE_PATH="$PROJECT_ROOT/apps/app/android/app/build.gradle"
+readonly APP_BUILD_GRADLE_PATH="$PROJECT_ROOT/apps/app/android/app/build.gradle.kts"
 
 sed -i '' "s/namespace \"$OLD_NAMESPACE\"/namespace \"$NEW_ANDROID_APPLICATION_ID\"/" "$APP_BUILD_GRADLE_PATH"
 
